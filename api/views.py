6dc2338b172ff41bb5c056b465e102e402313f3e
Caio de Faria <caiofaria2308@gmail.com>
2021-07-23 13:21:00 -0300

    fix bug authentication

--- api/views.py
+++ api/views.py
@@ -2,32 +2,38 @@ from person.models import Person, PersonMediaType, PersonMedia, PersonType, Pers
 from .serializers import PersonAuditSerializer, PersonMediaSerializer, PersonMediaTypeSerializer, PersonMediaTypeSerializer, PersonSerializer, PersonTypeSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
 class PersonTypeViewSet(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = PersonType.objects.all()
     serializer_class = PersonTypeSerializer
 
 
 class PersonMediaTypeViewSet(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = PersonMediaType.objects.all()
     serializer_class = PersonMediaTypeSerializer
 
 
 class PersonViewSet(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
 
 
 class PersonMediaViewSet(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = PersonMedia.objects.all()
     serializer_class = PersonMediaSerializer
 
 
 class PersonAuditViewSet(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = PersonAudit.objects.all()
     serializer_class = PersonAuditSerializer
--- suari/settings.py
+++ suari/settings.py
@@ -127,5 +127,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
     ]
 }
